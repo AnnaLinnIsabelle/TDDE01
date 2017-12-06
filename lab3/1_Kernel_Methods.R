c98f15ad8886a47be1ee1972b6460d26f6706273
Linn Abrahamsson <linab708@student.liu.se>
2017-12-06 21:26:37 +0100

    lab3_1 finished

--- lab3/1_Kernel_Methods.R
+++ lab3/1_Kernel_Methods.R
@@ -1,51 +1,59 @@
 set.seed(1234567890)
 library(geosphere)
 
+# Reading and formatting data
 stations <- read.csv("stations.csv", header=TRUE, sep=",", dec=".", fileEncoding = "latin1")
 temps <- read.csv("temps50k.csv")
 st <- merge(stations, temps, by="station_number")
-
 st = st[,c("date", "time", "latitude", "longitude", "air_temperature")]
 st$date = as.Date(st$date)
-date <- as.Date("2013-11-04") #The date to predict (up to the students)
-filtered_st = st[st$date < date, ]
-filtered_st$time = strptime(filtered_st$time, "%H:%M:%S")
 
-#Set all dates to year 2017
-filtered_st$date = as.Date(sub('\\d{4}(?=-)', '2000', filtered_st$date, perl=TRUE))
+# Prediction information
+date <- as.Date("2013-12-04") #The date to predict
+a <- 58.4274 # The latitude of point to predict
+b <- 14.826 # The longitude of point to predict
 
-#calculates distance in days between a reference date as a string and a date in format date
-dist_date <- function(refDate, date){
-  return (as.numeric(difftime(date, as.Date(refDate), units = "days")))
-}
-
-#calculates distance in minuites between a reference time given as string and a time given as strptime
-dist_time <- function(refTime, time) {
-  return(as.numeric(difftime(time, strptime(refTime, "%H:%M:%S"), units = "mins")))
-}
-
-h_distance <- sd(distHaversine(c(0,0), cbind(filtered_st$longitude, filtered_st$latitude)))# These three values are up to the students
-h_date <- sd(dist_date("2000-01-01", filtered_st$date))
-h_time <- sd(dist_time("00:00:00", filtered_st$time))
-
-
-a <- 58.4274 # The point to predict (up to the students)
-b <- 14.826
-
+# The times for prediction
 times <- c("04:00:00", "06:00:00", "08:00:00", "10:00:00", "12:00:00", "14:00:00", "16:00:00",
            "18:00:00", "20:00:00", "22:00:00", "24:00:00")
+# Change format of times to predict
+times <- strptime(times, "%H:%M:%S")
 
+# Remove dates after the date to predict
+filtered_st = st[st$date < date, ]
+# Change format of times in filtered data
+filtered_st$time = strptime(filtered_st$time, "%H:%M:%S") 
+
+# Predicted temperatures
 temp <- vector(length = length(times))
 
-#Students code here
+# Calculates distance in days between a reference date and a date
+dist_date <- function(refDate, date){
+  return (as.numeric(difftime(date, refDate, units = "days"))%%365)
+}
+
+# Calculates distance in minutes between a reference time and a time
+dist_time <- function(refTime, time) {
+  return(as.numeric(difftime(time, refTime, units = "mins")))
+}
+
+# Set smoothing factors
+h_distance <- sd(distHaversine(c(0,0), cbind(filtered_st$longitude, filtered_st$latitude)))
+h_date <- sd(dist_date(as.Date("2000-01-01"), filtered_st$date))
+h_time <- sd(dist_time(strptime("00:00:00", "%H:%M:%S"), filtered_st$time))
+
+
+# General formula for Gaussian Kernel
 gaussianKernel <- function(xDiff, h) {
   u <- xDiff/h
   return (exp(-(u)^2)) 
 }
+
+
+# Prediction with summation of kernels
 k1 <- gaussianKernel(distHaversine(c(b,a), cbind(filtered_st$longitude, filtered_st$latitude)), h_distance)
 
-pred_date <- as.Date(sub('\\d{4}(?=-)', '2000', date, perl=TRUE))
-k2 <- gaussianKernel(dist_date(pred_date, filtered_st$date), h_date)
+k2 <- gaussianKernel(dist_date(date, filtered_st$date), h_date)
 
 for (i in 1:length(times)) {
   k3 <- gaussianKernel(dist_time(times[i], filtered_st$time), h_time)
@@ -53,22 +61,29 @@ for (i in 1:length(times)) {
   temp[i] <-  sum((k1 + k2 + k3)*filtered_st$air_temperature) / sum(k1+k2+k3)
 }
 
-plot(temp, type="o")
+plot(times, temp, type="o", main="k1 + k2 + k3", col="red")
 
-#Time to validate if h-values are reasonable
-st_2013_11_04 = filtered_st[dist_date("2000-11-04", filtered_st[,'date']) == 0 ,]
+# Validate if h-values are reasonable
+close_date = filtered_st[(dist_date(date, filtered_st[,'date']) == 0 ) | 
+                           (dist_date(date, filtered_st[,'date']) == 364 )|
+                           (dist_date(date, filtered_st[,'date']) == 1 ),]
+geo_distances = distHaversine(c(b,a), cbind(close_date$longitude, close_date$latitude)) 
 
-minTimes <- numeric(length(times))
-tmp <- strptime(times, "%H:%M:%S")
-for (i in 1:length(times)) {
-  minTimes[i] <- dist_time("00:00:00", tmp[i])
-}
+rbPal <- colorRampPalette(c('red','yellow'))
+col <- rbPal(10)[as.numeric(cut(geo_distances,breaks = 40))]
 
-plot(minTimes, temp, xlab="minuites since midnight", type="o")
+plot(times, temp, type="o", ylim=c(min(temp)-8,max(temp)+8), main="Comparison to training data k1+k2+k3", col="red")
+points(close_date$time, close_date$air_temperature, col=col)
 
+
+# Prediction with multiplication of kernels
 for (i in 1:length(times)) {
   k3 <- gaussianKernel(dist_time(times[i], filtered_st$time), h_time)
   
   temp[i] <-  sum((k1 * k2 * k3)*filtered_st$air_temperature) / sum(k1*k2*k3)
 }
-plot(minTimes, temp, xlab="minuites since midnight", type="o")
+plot(times, temp, type="o", main="k1 * k2 * k3", col="red")
+
+# Validate if h-values are reasonable
+plot(times, temp, type="o", ylim=c(min(temp)-8,max(temp)+8), main="Comparison to training data k1*k2*k3", col="red")
+points(close_date$time, close_date$air_temperature, col=col)
